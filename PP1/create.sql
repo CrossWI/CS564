8a631f5edc1bff3504e736dd244bfe79d313f6f5
Cam <camcross11@gmail.com>
2021-10-05 21:58:14 -0500

    First commit

---
+++ PP1/create.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS Items;
+DROP TABLE IF EXISTS ItemBids;
+DROP TABLE IF EXISTS Bids;
+DROP TABLE IF EXISTS Users
+DROP TABLE IF EXISTS Category;
+
+CREATE TABLE Items(ItemID PRIMARY KEY INTEGER,
+				   Name CHAR(300),
+				   Started TIMESTAMP,
+				   Ends TIMESTAMP,
+				   SellerID CHAR(300),
+				   Description TEXT);
+
+CREATE TABLE ItemBids(ItemID PRIMARY KEY INTEGER,
+					  Currently DECIMAL,
+					  First_Bid DECIMAL,
+					  Number_of_Bids INTEGER
+					  FOREIGN KEY (ItemID) REFERENCES Items(ItemID));
+
+CREATE TABLE Bids(ItemID INTEGER,
+				  UserID CHAR(300),
+				  Time TIMESTAMP,
+				  Amount DECIMAL,
+				  FOREIGN KEY (ItemID) REFERENCES Items(ItemID),
+				  FOREIGN KEY (UserID) REFERENCES Users(UserID));
+
+CREATE TABLE Users(UserID CHAR(300),
+				   Location CHAR(300),
+				   Country CHAR(300),
+				   Rating INTEGER);
+
+CREATE TABLE Category(ItemID INTEGER,
+					  Category CHAR(300),
+					  FOREIGN KEY (ItemID) REFERENCES Items(ItemID))
