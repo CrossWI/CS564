ddeaea0186282aad5b894d9c67711b3fa2e56046
rich891216 <rich89126@gmail.com>
2021-10-09 15:51:50 -0500

    modified schema

--- PP1/create.sql
+++ PP1/create.sql
@@ -1,37 +1,40 @@
 DROP TABLE IF EXISTS Items;
-DROP TABLE IF EXISTS ItemBids;
 DROP TABLE IF EXISTS Bids;
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Category;
+DROP TABLE IF EXISTS Categories;
 
-CREATE TABLE Items(ItemID INTEGER PRIMARY KEY,
+CREATE TABLE Items(ItemID int PRIMARY KEY,
 				   Name CHAR(300),
 				   Started TIMESTAMP,
 				   Ends TIMESTAMP,
+				   Currently DECIMAL,
 				   SellerID CHAR(300),
-				   Description TEXT,
+				   Description CHAR(300),
+				   First_Bid DECIMAL,
+				   Number_of_Bids int,
+				   BuyPrice DECIMAL,
 				   FOREIGN KEY (SellerID) REFERENCES Users(UserID));
 
-CREATE TABLE ItemBids(ItemID INTEGER PRIMARY KEY,
-					  Currently DECIMAL,
-					  First_Bid DECIMAL,
-					  Number_of_Bids INTEGER,
-					  FOREIGN KEY (ItemID) REFERENCES Items(ItemID));
-
-CREATE TABLE Bids(ItemID INTEGER,
-				  UserID CHAR(300),
+CREATE TABLE Bids(ItemID int,
+				  BidderID CHAR(300),
 				  Time TIMESTAMP,
 				  Amount DECIMAL,
 				  FOREIGN KEY (ItemID) REFERENCES Items(ItemID),
-				  FOREIGN KEY (UserID) REFERENCES Users(UserID),
-				  PRIMARY KEY (ItemID, UserID, Amount));
+				  FOREIGN KEY (BidderID) REFERENCES Users(UserID),
+				  PRIMARY KEY (ItemID, Time));
 
 CREATE TABLE Users(UserID CHAR(300) PRIMARY KEY,
 				   Location CHAR(300),
 				   Country CHAR(300),
-				   Rating INTEGER);
+				   Rating int,
+				   isSeller int,
+				   isBidder int);
 
-CREATE TABLE Category(ItemID INTEGER,
-					  Category_Names CHAR(300),
+CREATE TABLE ItemCategory(ItemID int,
+					  CategoryID CHAR(300),
 					  FOREIGN KEY (ItemID) REFERENCES Items(ItemID),
-					  PRIMARY KEY (ItemID, Category_Names));
+					  FOREIGN KEY (CategoryID) REFERENCES Categories(CategoryID),
+					  PRIMARY KEY (ItemID, Category_Name));
+CREATE TABLE Categories(CategoryID int PRIMARY KEY,
+						Name CHAR(300));
